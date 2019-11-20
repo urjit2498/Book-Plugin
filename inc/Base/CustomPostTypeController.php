a3c77aad3c83185140b860483255ca7b89904a7d
urjit2498 <urjit64@gmail.com>
2019-11-20 18:55:30 +0530

    Modify some files for store custom post types.

--- inc/Base/CustomPostTypeController.php
+++ inc/Base/CustomPostTypeController.php
@@ -7,29 +7,49 @@ namespace Inc\Base;
 
 use Inc\Api\SettingsApi;
 use \Inc\Base\BaseController;
+use Inc\Api\Callbacks\CptCallbacks;
 use Inc\Api\Callbacks\AdminCallbacks;
-
 
 /**
  * 
  */
 class CustomPostTypeController extends BaseController
 {
-    public $subpages = array();
+    public $settings;
+
     public $callbacks;
+
+    public $cpt_callbacks;
+
+    public $subpages = array();
+
+    public $custom_post_types = array();
+    
     public function register()
     {
         if(! $this->activated('cpt_manager')) return;
         
         $this->callbacks = new AdminCallbacks();
 
+        $this->cpt_callbacks = new CptCallbacks();
+
         $this->settings = new SettingsApi();
         
         $this->setSubpages();
-
         $this->settings->addSubPages($this->subpages)->register();
         
-        add_action('init', array($this, 'activate'));
+        $this->setSettings();
+		$this->setSections();
+        $this->setFields();
+
+        
+        
+        $this->storeCustomPostTypes();
+        
+        if( ! empty( $this->custom_post_types ) ){
+            add_action('init', array($this, 'registerCustomPostTypes'));
+        }
+        
         
     }
 
@@ -47,17 +67,208 @@ class CustomPostTypeController extends BaseController
         );
     }
 
-    public function activate()
+    public function setSettings()
     {
-        register_post_type('book_product',
+        $args = array(
             array(
-                'labels' => array(
-                    'name'=>'Products',
-                    'singular_name'=>'Products' 
-                ),
-                'public'=>true,
-                'has_archive'=>true,
+                'option_group' => 'book_plugin_cpt_settings',
+                'option_name' => 'book_plugin_cpt',
+                'callback' => array($this->cpt_callbacks, 'cptSanitize')
             )
         );
+
+        $this->settings->setSettings($args);
+    }
+
+    public function setSections()
+    {
+        $args = array(
+            array(
+                'id' => 'book_cpt_index',
+                'title' => 'Custom Post Type Manager',
+                'callback' => array($this->cpt_callbacks, 'cptSectionManager'),
+                'page' => 'book_cpt'
+            )
+        );
+
+        $this->settings->setSections( $args );
+    }
+
+    public function setFields()
+    {
+        $args = array(
+            array(
+                'id' => 'post_type',
+                'title' => 'Custom Post Type ID',
+                'callback' => array($this->cpt_callbacks, 'textField'),
+                'page' => 'book_cpt',
+                'section' => 'book_cpt_index',
+                'args' => array(
+                    'option_name'=> 'book_plugin_cpt',
+                    'label_for' => 'post_type',
+                    'placeholder' => 'eg. product'
+                )
+            ),
+            array(
+                'id' => 'singular_name', 
+                'title' => 'Singular Name',
+                'callback' => array($this->cpt_callbacks, 'textField'),
+                'page' => 'book_cpt',
+                'section' => 'book_cpt_index',
+                'args' => array(
+                    'option_name'=> 'book_plugin_cpt',
+                    'label_for' => 'singular_name',
+                    'placeholder' => 'eg. Product'
+                )
+            ),
+            array(
+                'id' => 'plural_name', 
+                'title' => 'Plural Name',
+                'callback' => array($this->cpt_callbacks, 'textField'),
+                'page' => 'book_cpt',
+                'section' => 'book_cpt_index',
+                'args' => array(
+                    'option_name'=> 'book_plugin_cpt',
+                    'label_for' => 'plural_name',
+                    'placeholder' => 'eg. Products'
+                )
+            ),
+            array(
+                'id' => 'public',
+                'title' => 'Public',
+                'callback' => array($this->cpt_callbacks, 'checkboxField'),
+                'page' => 'book_cpt',
+                'section' => 'book_cpt_index',
+                'args' => array(
+                    'option_name'=> 'book_plugin_cpt',
+                    'label_for' => 'public',
+                    'class' => 'ui-toggle'
+                )
+            ),
+            array(
+                'id' => 'has_archive',
+                'title' => 'Archive',
+                'callback' => array($this->cpt_callbacks, 'checkboxField'),
+                'page' => 'book_cpt',
+                'section' => 'book_cpt_index',
+                'args' => array(
+                    'option_name'=> 'book_plugin_cpt',
+                    'label_for' => 'has_archive',
+                    'class' => 'ui-toggle'
+                )
+            )
+        );
+  
+        $this->settings->setFields($args)->register();
+    }
+
+    public function storeCustomPostTypes()
+    {
+        $items = get_option('book_plugin_cpt');
+        
+        // foreach ($items as $item) {
+            
+            $this->custom_post_types[] = array(
+                'post_type'             => $items['post_type'],
+                'name'                  => $items['plural_name'],
+                'singular_name'         => $items['plural_name'],
+                'menu_name'             => $items['plural_name'],
+                'name_admin_bar'        => $items['plural_name'],
+                'archives'              => $items['singular_name'].' Archive',
+                'attributes'            => $items['singular_name'].' Attributes',
+                'parent_item_colon'     => 'Parent '.$items['singular_name'],
+                'all_items'             => 'All '.$items['plural_name'],
+                'add_new_item'          => 'Add New '.$items['singular_name'],
+                'add_new'               => 'Add New',
+                'new_item'              => 'New '.$items['singular_name'],
+                'edit_item'             => 'Edit '.$items['singular_name'],
+                'update_item'           => 'Update '.$items['singular_name'],
+                'view_item'             => 'View '.$items['singular_name'],
+                'view_items'            => 'View '.$items['plural_name'],
+                'search_items'          => 'Search '.$items['plural_name'],
+                'not_found'             => 'No '.$items['singular_name'].' Found' ,
+                'not_found_in_trash'    => 'No '.$items['singular_name'].' Found in Tarsh',
+                'featured_image'        => 'Featured Image',
+                'set_featured_image'    => 'Set Featured Image',
+                'remove_featured_image' => 'Remove Featured Image',
+                'use_featured_image'    => 'Use Featured Image',
+                'insert_into_item'      => 'Insert Into '.$items['singular_name'],
+                'uploaded_to_this_item' => 'Upload to this '.$items['singular_name'],
+                'items_list'            => $items['plural_name'].' Lists',
+                'items_list_navigation' => $items['plural_name'].' Lists Navigations',
+                'filter_items_list'     => 'Filtered '.$items['plural_name'].' Lists',
+                'label'                 => $items['singular_name'],
+                'description'           => $items['plural_name'].' Custom Post Type',
+                'supports'              => array('title','editor','thumbnail'),
+                'taxonomies'            => array('Category','post_tag'),
+                'hierarchical'          => false,
+                'public'                => $items['public'],
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => true,
+                'can_export'            => true,
+                'has_archive'           => $items['has_archive'],
+                'exclude_from_search'   => false,
+                'publicly_queryable'    => true,
+                'capability_type'       => 'post'
+            );
+        // }
+        
+    }
+    public function registerCustomPostTypes()
+    {
+        foreach($this->custom_post_types as $post_type){
+            register_post_type($post_type['post_type'],
+                array(
+                    'labels' => array(
+                        'name'                  => $post_type['name'],
+                        'singular_name'         => $post_type['singular_name'],
+                        'menu_name'             => $post_type['menu_name'],
+                        'name_admin_bar'        => $post_type['name_admin_bar'],
+                        'archives'              => $post_type['archives'],
+                        'attributes'            => $post_type['attributes'],
+                        'parent_item_colon'     => $post_type['parent_item_colon'],
+                        'all_items'             => $post_type['all_items'],
+                        'add_new_item'          => $post_type['add_new_item'],
+                        'add_new'               => $post_type['add_new'],
+                        'new_item'              => $post_type['new_item'],
+                        'edit_item'             => $post_type['edit_item'],
+                        'update_item'           => $post_type['update_item'],
+                        'view_item'             => $post_type['view_item'],
+                        'view_items'            => $post_type['view_items'],
+                        'search_items'          => $post_type['search_items'],
+                        'not_found'             => $post_type['not_found'],
+                        'not_found_in_trash'    => $post_type['not_found_in_trash'],
+                        'featured_image'        => $post_type['featured_image'],
+                        'set_featured_image'    => $post_type['set_featured_image'],
+                        'remove_featured_image' => $post_type['remove_featured_image'],
+                        'use_featured_image'    => $post_type['use_featured_image'],
+                        'insert_into_item'      => $post_type['insert_into_item'],
+                        'uploaded_to_this_item' => $post_type['uploaded_to_this_item'],
+                        'items_list'            => $post_type['items_list'],
+                        'items_list_navigation' => $post_type['items_list_navigation'],
+                        'filter_items_list'     => $post_type['filter_items_list']
+                    ),
+                    'label'                     => $post_type['label'],
+                    'description'               => $post_type['description'],
+                    'supports'                  => $post_type['supports'],
+                    'taxonomies'                => $post_type['taxonomies'],
+                    'hierarchical'              => $post_type['hierarchical'],
+                    'public'                    => $post_type['public'],
+                    'show_ui'                   => $post_type['show_ui'],
+                    'show_in_menu'              => $post_type['show_in_menu'],
+                    'menu_position'             => $post_type['menu_position'],
+                    'show_in_admin_bar'         => $post_type['show_in_admin_bar'],
+                    'show_in_nav_menus'         => $post_type['show_in_nav_menus'],
+                    'can_export'                => $post_type['can_export'],
+                    'has_archive'               => $post_type['has_archive'],
+                    'exclude_from_search'       => $post_type['exclude_from_search'],
+                    'publicly_queryable'        => $post_type['publicly_queryable'],
+                    'capability_type'           => $post_type['capability_type']
+                )
+            );
+        }
     }
 }
--- templates/cpt.php
+++ templates/cpt.php
@@ -1,1 +1,12 @@
-<h1>Custom Post Type Manager</h1>
+<div class="wrap">
+    <h1>Custom Post Type Manager</h1>
+    <?php settings_errors(); ?>
+
+    <form method="post" action="options.php">
+        <?php
+            settings_fields('book_plugin_cpt_settings');
+            do_settings_sections('book_cpt');
+            submit_button();
+        ?>
+    </form>
+</div>
