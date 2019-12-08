3dfef9544a8f1ccf445d9050f17b7715d42867a0
urjit2498 <urjit64@gmail.com>
2019-12-08 23:04:54 +0530

    Adding Widget.

--- inc/Base/WidgetController.php
+++ inc/Base/WidgetController.php
@@ -5,9 +5,8 @@
 
 namespace Inc\Base;
 
-use Inc\Api\SettingsApi;
-use \Inc\Base\BaseController;
-use Inc\Api\Callbacks\AdminCallbacks;
+use Inc\Base\BaseController;
+use Inc\Api\Widgets\MediaWidget;
 
 
 /**
@@ -15,35 +14,10 @@ use Inc\Api\Callbacks\AdminCallbacks;
  */
 class WidgetController extends BaseController
 {
-    public $callbacks;
-
-    public $subpages = array();
-    
-    public function register()
-    {
-        
-        if(! $this->activated('media_widget')) return;
-
-        $this->settings = new SettingsApi();
-
-        $this->callbacks = new AdminCallbacks();
-        
-        $this->setSubpages();
-
-        $this->settings->addSubPages($this->subpages)->register();
-    }
-
-    public function setSubpages()
-    {
-        $this->subpages = array(
-            array(
-                'parent_slug' => 'book_plugin',
-                'page_title' => 'Widget Manager',
-                'menu_title' => 'Widget Manager',
-                'capability' => 'manage_options',
-                'menu_slug' => 'book_widget',
-                'callback' => array( $this->callbacks, 'adminWidget' )
-            ),
-        );
-    }
+	public function register()
+	{
+		if ( ! $this->activated( 'media_widget' ) ) return;
+		$media_widget = new MediaWidget();
+		$media_widget->register();
+	}
 }
