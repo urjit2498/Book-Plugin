8a2b1aea472c6aa4930448b931c36ca13bb8461e
urjit2498 <urjit64@gmail.com>
2019-12-10 16:33:24 +0530

    Create Custom Widget.

--- inc/Api/Widgets/MediaWidget.php
+++ inc/Api/Widgets/MediaWidget.php
@@ -13,14 +13,11 @@ use WP_Widget;
 class MediaWidget extends WP_Widget
 {
     public $widget_ID;
+	public $widget_name;
+	public $widget_options = array();
+	public $control_options = array();
 
-    public $widget_name;
-    
-    public $widget_options = array();
-    
-    public $control_options = array();
-
-    public function __construct()
+    function  __construct()
     {
         $this->widget_ID = 'book_media_widget';
 
@@ -28,58 +25,55 @@ class MediaWidget extends WP_Widget
 
         $this->widget_options = array(
             'classname' => $this->widget_ID,
-            'description' => $this->widget_name,
-            'customize_selective_refresh' => true,
+			'description' => $this->widget_name,
+			'customize_selective_refresh' => true,
         );
 
         $this->control_options = array(
-            'width' => 400,
-            'height' =>350,
-        );
+			'width' => 400,
+			'height' => 350,
+		);
     }
 
     public function register()
-    {
-        parent::__construct( $this->widget_ID, $this->widget_name, $this->widget_options, $this->control_options );
-
-        add_action('widget_init', array($this, 'widgetsInit'));
-    }
+	{
+		parent::__construct( $this->widget_ID, $this->widget_name, $this->widget_options, $this->control_options );
+		add_action( 'widgets_init', array( $this, 'widgetsInit' ) );
+	}
 
     public function widgetsInit()
-    {
-        register_widget( $this );
+	{
+		register_widget( $this );
     }
-
+    
     // Widget()
-    public function widget($args, $instance)
-    {
-        echo $args['before_widget'];
+    public function widget( $args, $instance ) {
+		echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
 		echo $args['after_widget'];
-    }
+	}
     
 
     // form()
     public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Custom Text', 'Book-Plugin' );
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Custom Text', 'awps' );
 		?>
 <p>
     <label
-        for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'Book-Plugin' ); ?></label>
+        for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'awps' ); ?></label>
     <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
         name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text"
         value="<?php echo esc_attr( $title ); ?>">
 </p>
 <?php 
-    }
-    
+	}
     //update()
     public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 		$instance['title'] = sanitize_text_field( $new_instance['title'] );
 		return $instance;
-    }
+	}
     
 }
--- inc/Base/WidgetController.php
+++ inc/Base/WidgetController.php
@@ -4,14 +4,11 @@
  */
 
 namespace Inc\Base;
-
 use Inc\Base\BaseController;
 use Inc\Api\Widgets\MediaWidget;
-
-
 /**
- * 
- */
+* 
+*/
 class WidgetController extends BaseController
 {
 	public function register()
