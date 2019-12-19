819f7dc04a1f12627eb01c9e3aa171ce47a0e9ad
urjit2498 <urjit64@gmail.com>
2019-12-19 22:05:00 +0530

    Fix some bugs and create sortable code in testimonial.

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -16,9 +16,13 @@ class TestimonialController extends BaseController
 	public function register()
 	{
 		if ( ! $this->activated( 'testimonial_manager' ) ) return;
+
 		add_action( 'init', array( $this, 'testimonial_cpt' ) );
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
 		add_action( 'save_post', array( $this, 'save_meta_box' ) );
+		add_action( 'manage_testimonial_posts_columns', array( $this, 'set_custom_columns' ) );
+		add_action( 'manage_testimonial_posts_custom_column', array( $this, 'set_custom_columns_data' ), 10, 2 );
+		add_filter( 'manage_edit-testimonial_sortable_columns', array( $this, 'set_custom_columns_sortable' ) );
 	}
 	public function testimonial_cpt ()
 	{
@@ -69,7 +73,7 @@ class TestimonialController extends BaseController
 </p>
 <div class="meta-container">
     <label class="meta-label w-50 text-left" for="book_testimonial_approved">Approved</label>
-    <div class="text-right w-50 inline">
+    <div class="text-right w-50 inline" style="display:inline-block;">
         <div class="ui-toggle inline"><input type="checkbox" id="book_testimonial_approved"
                 name="book_testimonial_approved" value="1" <?php echo $approved ? 'checked' : ''; ?>>
             <label for="book_testimonial_approved">
@@ -80,7 +84,7 @@ class TestimonialController extends BaseController
 </div>
 <div class="meta-container">
     <label class="meta-label w-50 text-left" for="book_testimonial_featured">Featured</label>
-    <div class="text-right w-50 inline">
+    <div class="text-right w-50" style="display:inline-block;">
         <div class="ui-toggle inline"><input type="checkbox" id="book_testimonial_featured"
                 name="book_testimonial_featured" value="1" <?php echo $featured ? 'checked' : ''; ?>>
             <label for="book_testimonial_featured">
@@ -113,5 +117,48 @@ class TestimonialController extends BaseController
 			'featured' => isset($_POST['book_testimonial_featured']) ? 1 : 0,
 		);
 		update_post_meta( $post_id, '_book_testimonial_key', $data );
+	}
+
+	public function set_custom_columns($columns)
+	{
+		$title = $columns['title'];
+		$date = $columns['date'];
+		unset( $columns['title'], $columns['date'] );
+
+		$columns['name'] = 'Author Name';
+		$columns['title'] = $title;
+		$columns['approved'] = 'Approved';
+		$columns['featured'] = 'Featured';
+		$columns['date'] = $date;
+		
+		return $columns;
+	}
+
+	public function set_custom_columns_data($column, $post_id)
+	{
+		$data = get_post_meta( $post_id, '_book_testimonial_key', true );
+		$name = isset($data['name']) ? $data['name'] : '';
+		$email = isset($data['email']) ? $data['email'] : '';
+		$approved = isset($data['approved']) && $data['approved'] === 1 ? '<strong>YES</strong>' : 'NO';
+		$featured = isset($data['featured']) && $data['featured'] === 1 ? '<strong>YES</strong>' : 'NO';
+		switch($column) {
+			case 'name':
+				echo '<strong>' . $name . '</strong><br/><a href="mailto:' . $email . '">' . $email . '</a>';
+				break;
+			case 'approved':
+				echo $approved;
+				break;
+			case 'featured':
+				echo $featured;
+				break;
+		}
+	}
+
+	public function set_custom_columns_sortable($columns)
+	{
+		$columns['name'] = 'name';
+		$columns['approved'] = 'approved';
+		$columns['featured'] = 'featured';
+		return $columns;
 	}
 }
